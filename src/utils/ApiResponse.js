996e27b1b80ffd372569f38a399bd98cb336203e
Harmain Rizwan <88616082+Harmain89@users.noreply.github.com>
2024-07-05 10:37:14 +0500

    understand this keyword in constructor.

--- src/utils/ApiResponse.js
+++ src/utils/ApiResponse.js
@@ -5,6 +5,8 @@ class ApiResponse {
         this.data = data
         this.message = message
         this.success = statusCode < 400
+
+        return this; // this keyword returns an object {} , and it may also be returned automatically if we do not return it manually.
     }
 }
 
